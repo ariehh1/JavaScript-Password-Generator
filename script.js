d5d3e9b895088350d00cb1b7a76cf8d18352ade4
Ari Horowitz <ariehh@comcast.net>
2019-12-08 22:07:45 -0800

    layout background pattern and setup

---
+++ script.js
@@ -0,0 +1,15 @@
+var upperCase = [ 'A', 'B']
+var specialCharacters = [ ]
+
+var totalArray = []
+console.log(totalArray);
+
+var password = []
+
+
+// Prompt length
+// Check  8 < length < 128
+// confirm hasSpecialCHaracters
+// confirm hasUpperCase
+// confirm hasLowerCase
+// confirm hasNumbers
