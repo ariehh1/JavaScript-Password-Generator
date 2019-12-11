95ea551afa58b5e205f4bb33203f513148968fc9
Ari Horowitz <ariehh@comcast.net>
2019-12-11 13:08:27 -0800

    README update

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # JavaScript-Password-Generator
+
+For my third repository on Github, I have created a Javascript Password Generator.  This application generates a random password based on user-selected criteria.  The application combines the HTML, CSS, and now Javascript tools we have been covering in the Bootcamp.  The user will be prompted to choose from the following:
+
+Length of password (must contain at least 8 characters but less than 128)
+
+Character Type:
+Special characters
+Numeric characters
+Lowercase characters
+Uppercase characters
+
+The end result of the application provides the user a new and secure password.  Without a set of prompts, the user would not be able to follow any directions towards generating a new password.  
+
+This repository cemented the concepts of Javascript and how logic plays a key role within software development.  By not confining a user within a set of boundaries and guidelines, the application would be open for interpretation and might not prompt the user towards a specific action or goal.
+
+References:
+
+https://www.w3schools.com/js/js_functions.asp
+https://getbootstrap.com/docs/4.4/components/alerts/
--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var specialCharacters = [" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
-var numeric = ['0', '1', '2', '3' , '4', '5', '6', '7', '8', '9']
+//variables
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var specialCharacters = [" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"];
+var numeric = ['0', '1', '2', '3' , '4', '5', '6', '7', '8', '9'];
+var questions = [
+    {q:'How many characters would you like your password to contain?', a: true},
+    {q:'Do you want to include special characters?', a: true}
+    
+];
+
+var generatePasswordbutton = document.getElementsByClassName("generate-password")[0];
+// generatePasswordbutton.addEventListener('click', function(event){
+//     event.preventDefault();
+//     console.log(event);
+// });
+
+generatePasswordbutton.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log(event);
+});
 
 var totalArray = []
-console.log(totalArray);
+    console.log(totalArray);
+    
+var password = []    
 
-var password = []
+//functions
+function generateUpper() {
+alert (length)
+}
+function generateLower() {
+
+}
+function generateSpecialCharacters() {
+
+}
+function generateNumeric() {
+
+}
+
 
 
 // Prompt length
@@ -14,4 +46,4 @@ var password = []
 // confirm hasSpecialCHaracters
 // confirm hasUpperCase
 // confirm hasLowerCase
-// confirm hasNumbers
+// confirm hasNumeric
