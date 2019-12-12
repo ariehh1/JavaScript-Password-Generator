3caf8b09e7f50105b8498d5a600a814c970f9f32
Ari Horowitz <ariehh@comcast.net>
2019-12-11 23:21:50 -0800

    new JS tests and updates to CSS

--- script.js
+++ script.js
@@ -1,89 +1,40 @@
-//variables
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var specialCharacters = [" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"];
-var numeric = ['0', '1', '2', '3' , '4', '5', '6', '7', '8', '9'];
-var questions = [
-    {q:'How many characters would you like your password to contain?', a: true},
-    {q:'Do you want to include special characters?', a: true}
-    {q:''}
-];
-
-//functions
 function generate(){
 
+    var questions = [
+        {q:'How many characters would you like your password to contain?', a: true},
+        {q:'Do you want to include special characters?', a: true},
+        {q:'Do you want to include numeric characters?', a: true},
+        {q:'Do you want to include lowercase characters?', a: true},
+        {q:'Do you want to include uppercase characters?', a: true},
+    ];
+
+    for var(i = 0; i < questions.length; i++);
+        var answer = confirm(questions[i].q);
+    
     var allcharacters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
-    var password = "";   
+    var password = "";
 
-    function randomize() {
-        for (var i = 0; i < allcharacters; i++) {
-             var allcharacters = Math.floor(Math.random() * totalArray.length);
-    }
-    
-    function randomize() {
-        var passwordArray = [];
-        for (var i = 0; i < allcharacters; i++) {
-             var allcharacters = Math.floor(Math.random() * totalArray.length);
-             passwordArray.push(allcharacters);
-        }
+    for(var i = 0; i <= allcharacters; i++){
+        password = password + allcharacters.charAt(Math.floor(Math.random() * Math.floor(allcharacters.length - 1)));
     }
 
-var generatePasswordbutton = document.getElementsByClassName("generate-password")[0];
-// generatePasswordbutton.addEventListener('click', function(event){
-//     event.preventDefault();
-//     console.log(event);
-// });
-
-generatePasswordbutton.addEventListener('click', function(event) {
-    event.preventDefault();
-    console.log(event);
-});
-
-var totalArray = []
-    console.log(totalArray);
-    
-   
-
-//functions
-function generateUpper() {
-alert (length)
-}
-function generateLower() {
-
-}
-
-function generateSpecialCharacters() {
-
-}
-
-function generateNumeric() {
-
-}
+    document.getElementById("display").value = password;
 
 function copytoClipboard() {
 
-    document.getElementById("display").select();
+    document.getElementById("exampleInputPassword1").select();
     
     document.execCommand("Copy");
 
-    alert("Your password has been copied to the clipboard!")
+    alert("Your password has now been copied to the clipboard")
 }
 
-function randomize() {
-    for (var i = 0; i < characters; i++) {
-         var chooseCharacters = Math.floor(Math.random() * totalArray.length);
-}
-
-function randomize() {
-    var passwordArray = [];
-    for (var i = 0; i < characters; i++) {
-         var chooseCharacters = Math.floor(Math.random() * totalArray.length);
-         passwordArray.push(chooseCharacters);
-    }
-}
-
-
+//variables
+//var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+//var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+//var specialCharacters = [" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"];
+//var numeric = ['0', '1', '2', '3' , '4', '5', '6', '7', '8', '9'];
 
 // Prompt length
 // Check  8 < length < 128
